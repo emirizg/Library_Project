833741f02d797426f428245bb194d3901f4ea940
Emirizg <emirizg77@gmail.com>
2022-12-20 15:54:01 +0100

    hooks updated

--- src/test/java/step_definitions/Hooks.java
+++ src/test/java/step_definitions/Hooks.java
@@ -1,4 +1,46 @@
 package step_definitions;
 
+import cucumber.api.Scenario;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import utilities.DBUtils;
+import utilities.Driver;
+
+import java.util.concurrent.TimeUnit;
+
 public class Hooks {
+
+    @Before("@db")
+    public void dbHook() {
+        System.out.println("creating database connection");
+        DBUtils.createConnection();
+    }
+
+    @After("@db")
+    public void afterDbHook() {
+        System.out.println("closing database connection");
+        DBUtils.destroyConnection();
+
+    }
+
+    @Before("@ui")
+    public void setUp() {
+        // we put a logic that should apply to every scenario
+        Driver.get().manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @After
+    public void tearDown(Scenario scenario) {
+        // only takes a screenshot if the scenario fails
+        if (scenario.isFailed()) {
+            // taking a screenshot
+            final byte[] screenshot = ((TakesScreenshot) Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
+        Driver.closeDriver();
+    }
+
 }
